af9a7551b84ac398b82ee5dce4454f2a10e36e96
Christopher Woggon <christopher.woggon@gmail.com>
2020-01-14 21:25:36 +0100

    Further cleanup

--- zerops-frontend/src/app/app.component.ts
+++ zerops-frontend/src/app/app.component.ts
@@ -17,82 +17,80 @@ import {uuidv4} from "./util/util";
 import {dataSourcesRaw, stepsRaw} from "./data/data";
 import {drawSvg} from "./util/d3Helper";
 
-function dataSourceLabelMatchesStepKeyValuePair(dataSourceLabel: DataSourceLabelKeyValuePair, stepKeyValuePair: StepKeyValuePair): boolean {
-  if (stepKeyValuePair.regex) {
-    let keyRegex: RegExp = RegExp(stepKeyValuePair.key);
-    if (!keyRegex.test(dataSourceLabel.key)) {
-      return false;
-    }
-    let valueRegex: RegExp = RegExp(stepKeyValuePair.value);
-    if (!valueRegex.test(dataSourceLabel.value)) {
-      return false;
-    }
-  } else {
-    if (dataSourceLabel.key !== stepKeyValuePair.key) {
-      return false;
-    }
-    if (dataSourceLabel.value !== stepKeyValuePair.value) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function stepMatchesDataSource(step: Step, dataSource: DataSource): boolean {
-  for (let stepKeyValuePair of step.keyValuePairs) {
-    let foundMatchingLabel: boolean = false;
-    for (let dataSourceLabel of dataSource.labels) {
-      if (dataSourceLabelMatchesStepKeyValuePair(dataSourceLabel, stepKeyValuePair)) {
-        foundMatchingLabel = true;
-        break;
-      }
-    }
-    if (!foundMatchingLabel) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function getDataSourceStepMatches(dataSources: string[], steps: string[]): DataSourceStepMatch[] {
-  let dataSourceStepMatches: DataSourceStepMatch[] = [];
-  for (let dataSource of dataSources) {
-    for (let step of steps) {
-      if (stepMatchesDataSource(stepMap.get(step), dataSourceMap.get(dataSource))) {
-        dataSourceStepMatches.push({dataSource: dataSource, step: step})
-      }
-    }
-  }
-  return dataSourceStepMatches;
-}
-
 function createKubernetesGraph(dataSources: DataSource[], steps: Step[]): KubernetesGraph {
-  let dataSourceGraphElements: DataSourceGraphElement[] = [];
-  dataSources.forEach(dataSource => dataSourceGraphElements.push({
-    uuid: dataSource.uuid,
-    stepGraphElements: [],
-    creatorStepGraphElement: null
-  }));
+  function getDataSourceStepMatches(dataSources: string[], steps: string[]): DataSourceStepMatch[] {
+    function stepMatchesDataSource(step: Step, dataSource: DataSource): boolean {
+      function dataSourceLabelMatchesStepKeyValuePair(dataSourceLabel: DataSourceLabelKeyValuePair, stepKeyValuePair: StepKeyValuePair): boolean {
+        if (stepKeyValuePair.regex) {
+          let keyRegex: RegExp = RegExp(stepKeyValuePair.key);
+          if (!keyRegex.test(dataSourceLabel.key)) {
+            return false;
+          }
+          let valueRegex: RegExp = RegExp(stepKeyValuePair.value);
+          if (!valueRegex.test(dataSourceLabel.value)) {
+            return false;
+          }
+        } else {
+          if (dataSourceLabel.key !== stepKeyValuePair.key) {
+            return false;
+          }
+          if (dataSourceLabel.value !== stepKeyValuePair.value) {
+            return false;
+          }
+        }
+        return true;
+      }
 
-  let stepGraphElements: StepGraphElement[] = [];
-  steps.forEach(step => stepGraphElements.push({
-    uuid: step.uuid,
-    outputDataSourceGraphElements: [],
-    sourceDataSourceGraphElements: []
-  }));
+      for (let stepKeyValuePair of step.keyValuePairs) {
+        let foundMatchingLabel: boolean = false;
+        for (let dataSourceLabel of dataSource.labels) {
+          if (dataSourceLabelMatchesStepKeyValuePair(dataSourceLabel, stepKeyValuePair)) {
+            foundMatchingLabel = true;
+            break;
+          }
+        }
+        if (!foundMatchingLabel) {
+          return false;
+        }
+      }
+      return true;
+    }
 
-  let dataSourceStepMatches: DataSourceStepMatch[] = getDataSourceStepMatches(dataSources.map(dataSource => dataSource.uuid), steps.map(step => step.uuid));
-  dataSourceStepMatches.forEach(match => {
-    let dataSourceGraphElement: DataSourceGraphElement = dataSourceGraphElements.filter(dataSourceGraphElement => dataSourceGraphElement.uuid === match.dataSource)[0];
-    let stepGraphElement: StepGraphElement = stepGraphElements.filter(stepGraphElement => stepGraphElement.uuid === match.step)[0];
+    let dataSourceStepMatches: DataSourceStepMatch[] = [];
+    for (let dataSource of dataSources) {
+      for (let step of steps) {
+        if (stepMatchesDataSource(stepMap.get(step), dataSourceMap.get(dataSource))) {
+          dataSourceStepMatches.push({dataSource: dataSource, step: step})
+        }
+      }
+    }
+    return dataSourceStepMatches;
+  }
+  function fillDataSourceGraphElementsArray(dataSources: DataSource[], dataSourceGraphElements: DataSourceGraphElement[]) {
+    dataSources.forEach(dataSource => dataSourceGraphElements.push({
+      uuid: dataSource.uuid,
+      stepGraphElements: [],
+      creatorStepGraphElement: null
+    }));
+  }
+  function fillStepGraphElementsArray(steps: Step[], stepGraphElements: StepGraphElement[]) {
+    steps.forEach(step => stepGraphElements.push({
+      uuid: step.uuid,
+      outputDataSourceGraphElements: [],
+      sourceDataSourceGraphElements: []
+    }));
+  }
+  function connectDataSourceStepMatchesInDataStructures(dataSourceStepMatches: DataSourceStepMatch[], dataSourceGraphElements: DataSourceGraphElement[], stepGraphElements: StepGraphElement[]) {
+    dataSourceStepMatches.forEach(match => {
+      let dataSourceGraphElement: DataSourceGraphElement = dataSourceGraphElements.filter(dataSourceGraphElement => dataSourceGraphElement.uuid === match.dataSource)[0];
+      let stepGraphElement: StepGraphElement = stepGraphElements.filter(stepGraphElement => stepGraphElement.uuid === match.step)[0];
 
-    dataSourceGraphElement.stepGraphElements.push(stepGraphElement.uuid);
-    stepGraphElement.sourceDataSourceGraphElements.push(dataSourceGraphElement.uuid);
-  });
-
-  stepGraphElements.forEach(stepGraphElement => {
-    let currentStep: Step = stepMap.get(stepGraphElement.uuid);
-    if (currentStep.type === AnalysisType.ONE_TO_ONE) {
+      dataSourceGraphElement.stepGraphElements.push(stepGraphElement.uuid);
+      stepGraphElement.sourceDataSourceGraphElements.push(dataSourceGraphElement.uuid);
+    });
+  }
+  function handleAllSteps(stepGraphElements: StepGraphElement[]) {
+    function handleOneToOneStep(stepGraphElement, currentStep: Step) {
       stepGraphElement.sourceDataSourceGraphElements.forEach(sourceDataSourceGraphElement => {
         currentStep.outputLabelsArray.forEach(outputLabels => {
           let currentDataSource: DataSource = dataSourceMap.get(sourceDataSourceGraphElement);
@@ -111,7 +109,8 @@ function createKubernetesGraph(dataSources: DataSource[], steps: Step[]): Kubern
           stepGraphElements.filter(stepGraphElement => stepGraphElement.uuid === currentStep.uuid)[0].outputDataSourceGraphElements.push(outputDataSource.uuid);
         });
       });
-    } else if (currentStep.type === AnalysisType.ALL_TO_ONE) {
+    }
+    function handleAllToOneStep(stepGraphElement, currentStep: Step) {
       let sourceDataSources: DataSource[] = [];
       stepGraphElement.sourceDataSourceGraphElements.forEach(sourceDataSource => sourceDataSources.push(dataSourceMap.get(sourceDataSource)));
       let labelIntersection: KeyValuePair[] = [];
@@ -146,25 +145,34 @@ function createKubernetesGraph(dataSources: DataSource[], steps: Step[]): Kubern
         });
         stepGraphElements.filter(stepGraphElement => stepGraphElement.uuid === currentStep.uuid)[0].outputDataSourceGraphElements.push(outputDataSource.uuid);
 
-      })
+      });
     }
-  });
 
+    stepGraphElements.forEach(stepGraphElement => {
+      let currentStep: Step = stepMap.get(stepGraphElement.uuid);
+      if (currentStep.type === AnalysisType.ONE_TO_ONE) {
+        handleOneToOneStep(stepGraphElement, currentStep);
+      } else if (currentStep.type === AnalysisType.ALL_TO_ONE) {
+        handleAllToOneStep(stepGraphElement, currentStep);
+      }
+    });
+  }
+  function buildKubernetesGraph(dataSourceGraphElements: DataSourceGraphElement[], stepGraphElements: StepGraphElement[]) {
+    return {
+      dataSourceGraphElements: dataSourceGraphElements,
+      stepGraphElements: stepGraphElements
+    };
+  }
 
-  return {
-    dataSourceGraphElements: dataSourceGraphElements,
-    stepGraphElements: stepGraphElements
-  };
-}
+  let dataSourceGraphElements: DataSourceGraphElement[] = [];
+  fillDataSourceGraphElementsArray(dataSources, dataSourceGraphElements);
+  let stepGraphElements: StepGraphElement[] = [];
+  fillStepGraphElementsArray(steps, stepGraphElements);
+  let dataSourceStepMatches: DataSourceStepMatch[] = getDataSourceStepMatches(dataSources.map(dataSource => dataSource.uuid), steps.map(step => step.uuid));
+  connectDataSourceStepMatchesInDataStructures(dataSourceStepMatches, dataSourceGraphElements, stepGraphElements);
+  handleAllSteps(stepGraphElements);
 
-function fillDataSourceMap(dataSources: DataSource[]): void {
-  dataSources.forEach(dataSource => {
-    dataSourceMap.set(dataSource.uuid, dataSource);
-  });
-}
-
-function fillStepMap(steps: Step[]): void {
-  steps.forEach(step => stepMap.set(step.uuid, step));
+  return buildKubernetesGraph(dataSourceGraphElements, stepGraphElements);
 }
 
 function getAllDataSources(): DataSource[] {
@@ -175,21 +183,9 @@ function getAllSteps(): Step[] {
   return Array.from(stepMap.keys()).map(stepKey => stepMap.get(stepKey));
 }
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent implements AfterContentInit {
-  title = 'zerops-frontend';
-
-  @HostListener('click', ['$event.target']) onClick(target) {
-    if (!target.closest('rect')) return;
-    alert(target.id);
-  }
-
-  ngAfterContentInit() {
-    let initialDataSources: DataSource[] = dataSourcesRaw.items.map(dataSourceRaw => ({
+function getRawDataSourcesAndSaveToMap() {
+  function getDataSourcesFromRawData() {
+    return dataSourcesRaw.items.map(dataSourceRaw => ({
       name: dataSourceRaw.metadata.name,
       labels: Object.keys(dataSourceRaw.metadata.labels).map(dataSourceLabelKey => ({
         key: dataSourceLabelKey,
@@ -198,8 +194,21 @@ export class AppComponent implements AfterContentInit {
       uuid: uuidv4(),
       depth: 0
     }));
-    fillDataSourceMap(initialDataSources);
-    let initialSteps: Step[] = stepsRaw.items.map(stepRaw => ({
+  }
+
+  function fillDataSourceMap(dataSources: DataSource[]): void {
+    dataSources.forEach(dataSource => {
+      dataSourceMap.set(dataSource.uuid, dataSource);
+    });
+  }
+
+  let initialDataSources: DataSource[] = getDataSourcesFromRawData();
+  fillDataSourceMap(initialDataSources);
+}
+
+function getRawStepsAndSaveToMap() {
+  function getStepsFromRawData() {
+    return stepsRaw.items.map(stepRaw => ({
       name: stepRaw.metadata.name,
       keyValuePairs: stepRaw.spec.ingest.map(ingest => ({
         regex: ingest.check === 'regex',
@@ -215,13 +224,31 @@ export class AppComponent implements AfterContentInit {
         }));
       })
     }));
-    fillStepMap(initialSteps);
-
-    let kubernetesGraph: KubernetesGraph = createKubernetesGraph(getAllDataSources(), getAllSteps());
-
-    drawSvg.call(this, kubernetesGraph);
-
   }
 
+  function fillStepMap(steps: Step[]): void {
+    steps.forEach(step => stepMap.set(step.uuid, step));
+  }
 
+  let initialSteps: Step[] = getStepsFromRawData();
+  fillStepMap(initialSteps);
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterContentInit {
+  @HostListener('click', ['$event.target']) onClick(target) {
+    if (!target.closest('rect')) return;
+    alert(target.id);
+  }
+
+  ngAfterContentInit() {
+    getRawDataSourcesAndSaveToMap();
+    getRawStepsAndSaveToMap();
+    let kubernetesGraph: KubernetesGraph = createKubernetesGraph(getAllDataSources(), getAllSteps());
+    drawSvg.call(this, kubernetesGraph);
+  }
 }
