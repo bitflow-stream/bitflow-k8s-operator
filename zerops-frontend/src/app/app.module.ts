f8fb31365efae42edf8bf50cb5d558b8b4355812
Christopher Woggon <christopher.woggon@gmail.com>
2020-02-12 16:22:53 +0100

    Normal modal works

--- zerops-frontend/src/app/app.module.ts
+++ zerops-frontend/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgbdModalBasic} from './modal-basic/modal-basic.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdModalBasic
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ zerops-frontend/src/app/modal-basic/modal-basic.component.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'ngbd-modal-basic',
+  templateUrl: './modal-basic.component.html'
+})
+export class NgbdModalBasic {
+  closeResult: string = '';
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+}
