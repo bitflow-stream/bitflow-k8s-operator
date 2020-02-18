5097e5a717340601f8d39c033e37b93b476d9883
Christopher Woggon <christopher.woggon@gmail.com>
2020-02-19 00:34:55 +0100

    Displays nodes

--- zerops-frontend/src/app/graph/graph.component.ts
+++ zerops-frontend/src/app/graph/graph.component.ts
@@ -1,7 +1,17 @@
 import {AfterContentInit, Component, HostListener, ViewChild} from "@angular/core";
 import {ConfigModalComponent} from "./config-modal/config-modal.component";
 import {drawSvg} from "../../externalized/util/d3Helper";
-import {DataSource, GraphVisualization, Pod, podMap, Step} from "../../externalized/definitions/definitions";
+import {
+  D3Edge,
+  D3Node,
+  DataSource,
+  FrontendData,
+  GraphVisualization,
+  GraphVisualizationColumn,
+  Pod,
+  podMap,
+  Step
+} from "../../externalized/definitions/definitions";
 import {
   getDataSourcesFromRawDataAndSaveToMap,
   getPodsAndStepsFromRawDataAndSaveToMap
@@ -11,12 +21,19 @@ import {
   getAllDataSources,
   getAllPods,
   getAllSteps,
+  getCurrentPods,
   getDepthOfGraphElement,
   setCurrentDataSources,
   setCurrentPods,
   setCurrentSteps
 } from "../../externalized/functionalities/quality-of-life-functions";
-
+import {
+  svgHorizontalGap,
+  svgNodeHeight,
+  svgNodeMargin,
+  svgNodeWidth, svgPodNodeMargin,
+  svgVerticalGap
+} from "../../externalized/config/config";
 
 function addCreatorPodsToDataSources() {
   getAllDataSources().forEach(dataSource => {
@@ -39,7 +56,7 @@ function setCurrentGraphElements(dataSources, steps, pods) {
 }
 
 function getGraphVisualization() {
-  let numberOfColumns = getAllCurrentGraphElements().map(element => {
+  let maxColumnId = getAllCurrentGraphElements().map(element => {
     return getDepthOfGraphElement(element);
   }).reduce((p, c) => {
     if (c == undefined) {
@@ -48,17 +65,87 @@ function getGraphVisualization() {
     return Math.max(p, c)
   });
 
-  console.log(numberOfColumns);
+  let graphVisualization: GraphVisualization = {graphColumns: []};
 
-  return undefined;
+  for (let i = 0; i <= maxColumnId; i++) {
+    graphVisualization.graphColumns.push({graphElements: []});
+  }
+
+  getAllCurrentGraphElements().forEach(element => {
+    let depth = getDepthOfGraphElement(element);
+    let graphVisualizationColumn: GraphVisualizationColumn = graphVisualization.graphColumns[depth];
+    if (element.type === 'step') {
+      graphVisualizationColumn.graphElements.push(element);
+      let currentPods = getCurrentPods();
+      let currentPodsInStep = element.step.pods.filter(pod => currentPods.some(currentPod => currentPod.name === pod.name));
+      currentPodsInStep.forEach(pod => graphVisualizationColumn.graphElements.push({type: 'pod', pod: pod}));
+    }
+    if (element.type === 'data-source') {
+      graphVisualizationColumn.graphElements.push(element);
+    }
+  });
+
+  return graphVisualization;
+}
+
+function getFrontendDataFromGraphVisualization(graphVisualization: GraphVisualization) {
+  let nodes: D3Node[] = [];
+  let edges: D3Edge[] = [];
+
+  graphVisualization.graphColumns.forEach((column, columnId) => {
+    let currentHeight = 0;
+    column.graphElements.forEach(graphElement => {
+      if (graphElement.type === 'data-source') {
+        nodes.push({
+          id: graphElement.dataSource.name,
+          text: graphElement.dataSource.name,
+          x: columnId * (svgNodeWidth + svgHorizontalGap) + svgNodeMargin,
+          y: currentHeight + svgNodeMargin,
+          width: svgNodeWidth,
+          height: svgNodeHeight,
+          type: "data-source"
+        });
+        currentHeight += svgNodeHeight + svgVerticalGap;
+      }
+      if (graphElement.type === 'pod') {
+        nodes.push({
+          id: graphElement.pod.name,
+          text: graphElement.pod.name,
+          x: columnId * (svgNodeWidth + svgHorizontalGap) + svgNodeMargin,
+          y: currentHeight + svgNodeMargin,
+          width: svgNodeWidth,
+          height: svgNodeHeight,
+          type: "pod"
+        });
+        currentHeight += svgNodeHeight + svgVerticalGap;
+      }
+      if (graphElement.type === 'step') {
+        if (graphElement.step.podType === 'pod') {
+          let currentPods = getCurrentPods();
+          let currentPodsInStep = graphElement.step.pods.filter(pod => currentPods.some(currentPod => currentPod.name === pod.name));
+          nodes.push({
+            id: graphElement.step.name,
+            text: graphElement.step.name,
+            x: columnId * (svgNodeWidth + svgHorizontalGap) - svgPodNodeMargin + svgNodeMargin,
+            y: currentHeight - svgPodNodeMargin + svgNodeMargin,
+            width: svgNodeWidth + 2 * svgPodNodeMargin,
+            height: Math.max(1, currentPodsInStep.length) * (svgNodeHeight + svgVerticalGap),
+            type: 'step'
+          });
+        }
+      }
+    });
+  });
+
+  return {nodes, edges} as FrontendData;
 }
 
 function displayGraph(this: any, dataSources: DataSource[], steps: Step[], pods: Pod[]): void {
   setCurrentGraphElements(dataSources, steps, pods);
 
   let graphVisualization: GraphVisualization = getGraphVisualization();
-  console.log(graphVisualization);
-  drawSvg.call(this, {nodes: [], edges: []});
+  let frontendData: FrontendData = getFrontendDataFromGraphVisualization(graphVisualization);
+  drawSvg.call(this, frontendData);
 }
 
 @Component({
--- zerops-frontend/src/externalized/definitions/definitions.ts
+++ zerops-frontend/src/externalized/definitions/definitions.ts
@@ -34,7 +34,6 @@ export interface GraphVisualization {
 }
 
 export interface GraphVisualizationColumn {
-  maxFilledRow: number;
   graphElements: GraphElement[];
 }
 
