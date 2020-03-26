23b4be64c9cd90cd5766e58d82c93281a960ca99
Anton Gulenko <anton.gulenko@googlemail.com>
2020-03-26 17:10:10 +0100

    debugging build

--- bitflow-api-proxy/build/build.sh
+++ bitflow-api-proxy/build/build.sh
@@ -20,6 +20,11 @@ build_args="$@"
 # Build inside the container, but mount relevant directories to get access to the build results.
 docker run -v "$mod_cache_dir:/go" -v "$root:/build/src" "$BUILD_IMAGE" \
   sh -c "
+    echo PWD
+    pwd
+    echo LISTING
+    ls
+
     # Copy entire source-tree in order to make changes to go.mod/go.sum
     cp -r src build
     cd build
