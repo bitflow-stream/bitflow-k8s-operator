fedff25224120ed34297c630665d6b27737ee847
Anton Gulenko <anton.gulenko@googlemail.com>
2020-03-26 10:57:25 +0100

    Moved a function to common package

--- bitflow-controller/go.mod
+++ bitflow-controller/go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/NYTimes/gziphandler v1.0.1 // indirect
 	github.com/antongulenko/go-onlinestats v0.0.0-20160514060630-5ff69410145c
 	github.com/antongulenko/golib v0.0.24
-	github.com/bitflow-stream/go-bitflow v0.0.55
+	github.com/bitflow-stream/go-bitflow v0.0.56
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-openapi/spec v0.19.0
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
--- bitflow-controller/pkg/common/helpers.go
+++ bitflow-controller/pkg/common/helpers.go
@@ -1,7 +1,24 @@
 package common
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"regexp"
+	"strings"
+
 	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	HashSuffixLength = 8
+
+	nonDns1123CharReplacement  = "-"
+	dns1123StartEndReplacement = "a" // DNS names must start and end with lower-case letters, so there is no real valid replacement for invalid characters...
+)
+
+var (
+	nonDns1123Chars = regexp.MustCompile("[^-a-z0-9.]") // a dot is added because it does not seem forbidden in Kubernetes, although it is not allows in DNS 1123
+	dns1123StartEnd = regexp.MustCompile("[a-z0-9]")
 )
 
 func IsBeingDeleted(pod *corev1.Pod) bool {
@@ -27,4 +44,31 @@ func GetNodeName(pod *corev1.Pod) string {
 	*/
 
 	return ""
+}
+
+func HashName(prefix string, hashInputStrings ...string) string {
+	hashInputString := strings.Join(hashInputStrings, "--")
+	hashString := fmt.Sprintf("%x", sha1.Sum([]byte(hashInputString)))
+	return CleanDnsName(prefix + hashString[:HashSuffixLength])
+}
+
+func CleanDnsName(name string) string {
+	// Prepare the given name to conform with DNS-1123, which is enforced by Kubernetes
+	if len(name) > 0 {
+		name = strings.ToLower(name)
+		name = nonDns1123Chars.ReplaceAllString(name, nonDns1123CharReplacement)
+
+		// Check if first and last chars conform to the spec
+		if !dns1123StartEnd.MatchString(name[0:1]) {
+			name = dns1123StartEndReplacement + name[1:]
+		}
+		lastChar := len(name) - 1
+		if !dns1123StartEnd.MatchString(name[lastChar : lastChar+1]) {
+			name = name[:lastChar] + dns1123StartEndReplacement
+		}
+	}
+
+	// TODO the resulting name might be too long for DNS-1123, which is enforced by Kubernetes and only allows names of 63 characters
+	// TODO if the name is too long, strip characters from the original prefix (middle or end), but preserve the UUID
+	return name
 }
