f8e0bd56f06b5beb4430f28c1bc750bd1adcd17a
Christopher Woggon <christopher.woggon@gmail.com>
2020-03-26 19:45:32 +0100

    Frontend -> Dashboard

--- bitflow-controller-dashboard/README.md
+++ bitflow-controller-dashboard/README.md
@@ -1,4 +1,4 @@
-# Bitflow Controller Frontend
+# Bitflow Controller Dashboard
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.17.
 
--- bitflow-controller-dashboard/build-docker-image-and-run.sh
+++ bitflow-controller-dashboard/build-docker-image-and-run.sh
@@ -1,2 +1,2 @@
-docker build -t teambitflow/bitflow-controller-frontend:v1 .
-docker run -p 80:80 teambitflow/bitflow-controller-frontend:v1
+docker build -t teambitflow/bitflow-controller-dashboard:v1 .
+docker run -p 80:80 teambitflow/bitflow-controller-dashboard:v1
