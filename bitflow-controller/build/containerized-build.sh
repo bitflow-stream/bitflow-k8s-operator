3cac477f607f1d2801d43091dddd9253a779850d
Christopher Woggon <christopher.woggon@gmail.com>
2020-04-19 11:28:05 +0200

    Fixing build scripts

--- bitflow-controller/build/containerized-build.sh
+++ bitflow-controller/build/containerized-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 home=`dirname $(readlink -f $0)`
-root=`readlink -f "$home/.."`
+root=`readlink -f "$home/../.."`
 
 test $# -ge 2 || { echo "Parameters: <container to build for (arm32v7/arm64v8/alpine)> <Go-mod-cache directory> <Build args (optional)>"; exit 1; }
 BUILD_TARGET="$1"
