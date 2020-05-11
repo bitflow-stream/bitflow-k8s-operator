2dcd480125337e06e3d8d5b90abff1c4cbf6409b
Christopher Woggon <christopher.woggon@gmail.com>
2020-05-11 10:36:32 +0200

    Implemented calculateExecutionTime() based on available resources

--- bitflow-controller/pkg/controller/bitflow/calculate_penalty.go
+++ bitflow-controller/pkg/controller/bitflow/calculate_penalty.go
@@ -2,13 +2,23 @@ package bitflow
 
 import (
 	"github.com/bitflow-stream/bitflow-k8s-operator/bitflow-controller/pkg/common"
+	"math"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const podPenalty = 5
 
+const a = -5
+const b = 0
+const c = -0.1
+const d = 7
+
 func getPodPenalty() int {
 	return podPenalty
+}
+
+func calculateExecutionTime(cpus float64) float64 {
+	return a*math.Pow(cpus+b, -c) + d
 }
 
 // lower is better
--- bitflow-controller/pkg/controller/bitflow/test_calculate_penalty_test.go
+++ bitflow-controller/pkg/controller/bitflow/test_calculate_penalty_test.go
@@ -1,11 +1,19 @@
 package bitflow
 
+import "math"
+
 type CalculatePenaltyTestSuite struct {
 	BitflowControllerTestHelpers
 }
 
 func (s *BitflowControllerTestSuite) TestCalculatePenalty() {
 	s.SubTestSuite(new(CalculatePenaltyTestSuite))
+}
+
+const float64EqualityThreshold = 1e-5
+
+func almostEqual(a, b float64) bool {
+	return math.Abs(a-b) <= float64EqualityThreshold
 }
 
 func (s *CalculatePenaltyTestSuite) TestCalculatePenaltyForNode() {
@@ -32,3 +40,20 @@ func (s *CalculatePenaltyTestSuite) TestCalculatePenaltyForNonExistingNode() {
 	s.NoError(err)
 	s.Equal(0, penalty)
 }
+
+func (s *CalculatePenaltyTestSuite) TestCalculateExecutionTimeOneCpu() {
+	executionTime := calculateExecutionTime(1)
+	s.Equal(2.0, executionTime)
+}
+
+func (s *CalculatePenaltyTestSuite) TestCalculateExecutionTimeTwoCpus() {
+	executionTime := calculateExecutionTime(2)
+	s.True(almostEqual(1.641133, executionTime))
+}
+
+func (s *CalculatePenaltyTestSuite) TestCalculateExecutionTimeHalfCpu() {
+	executionTime := calculateExecutionTime(0.5)
+	s.True(almostEqual(2.33484, executionTime))
+}
+
+// TODO test for very small cpu count + very high cpu count once actual curve parameters are defined
